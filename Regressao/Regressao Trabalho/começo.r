761dbd30708b004a2db8fdcf88675871ff0fb187
Celo <q2650473562@gmail.com>
2026-05-05 23:53:06 -0300

    add trabalho de regressao

---
+++ Regressao/Regressao Trabalho/começo.r
@@ -0,0 +1,12 @@
+library(help = 'datasets')
+?datasets
+mtcars
+?MPV
+install.packages("dslabs")
+library("dslabs")
+data("murders")
+library(tidyverse)
+View(murders)
+
+data("gapminder")
+View(gapminder)
